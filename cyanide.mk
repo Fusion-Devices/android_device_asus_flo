0666698306da2448801ea7fe53fb4f2637d7e57e
rogersb11 <brettrogers11@gmail.com>
2015-07-07 20:16:07 -0400

    We don't support half res because I'm lazy

--- cyanide.mk
+++ cyanide.mk
@@ -2,7 +2,6 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1200
-TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common CYANIDE stuff.
 $(call inherit-product, vendor/cyanide/config/common_full_tablet_wifionly.mk)
